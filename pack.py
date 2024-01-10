3afb2dce08871c6998108c0515d1562498ab4a4d
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2024-01-10 09:00:33 -0500

    .

--- pack.py
+++ pack.py
@@ -7,14 +7,33 @@ class Pack:
         self.members = []
         self.leader_index = 0
 
-        def get_leader_name():
-            print(self.members[self.leader_index].get_name())
+    def get_leader_name(self):
+        print('the leader is ', self.members[self.leader_index].get_name())
 
-        def add_member():
-            self.members.append('')
+    def add_member(self, new_member):
+        self.members.append(new_member)
 
-        def print_pack():
-            for x in range(len(self.members)):
-                print(self.members)
-        print_pack()
-        get_leader_name()
+    def print_pack(self):
+        # for x in range(len(self.members)):
+        for member in self.members:
+            print(member.get_name())
+
+    def new_leader(self, members):
+        print(members[3], 'ousts', members[0], 'from leadership.')
+
+
+dog1 = Dog('Dakota')
+dog2 = Dog('Lucy')
+dog3 = Dog('Sasha')
+dog4 = Dog('Franklin')
+
+pack1 = Pack(dog1)
+
+pack1.add_member(dog1)
+pack1.add_member(dog2)
+pack1.add_member(dog3)
+pack1.add_member(dog4)
+
+pack1.print_pack()
+pack1.get_leader_name()
+pack1.new_leader()
